f1aa1f22a4f397f57c0ddd0bf924a62e8029d8f8
pedro_bras <pedro.c.bras@gmail.com>
2019-09-14 11:26:29 +0200

    adds content to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,38 @@ import React from "react";
 import "../css/style.css";
 
 const Footer = () => {
-  return <div id="footer">Made with Love</div>;
+  return (
+    <div>
+      <div id="footer">
+      <div id="logo-bg">
+        <div id="phadder-logo" />
+      </div>
+      <div>
+        <h3>Contact</h3>
+        <p>Support</p>
+        <p>Facebook</p>
+        <p>Instagram</p>
+      </div>
+      <div>
+        <h3>Platform</h3>
+        <p>About</p>
+        <p>Pricing</p>
+      </div>
+      <div>
+        <h3>Resources</h3>
+        <p>Resources</p>
+        <p>User Stories</p>
+      </div>
+      <div>
+        <h3>News</h3>
+        <p>Latest News</p>
+      </div>
+      
+    </div>
+    <div>Made with Love</div>
+    </div>
+  );
+    
 };
 
 export default Footer;
